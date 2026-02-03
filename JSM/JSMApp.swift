3ad041388f84725914d5f2780721e61abdfaba10
dwgx <dwgx1337@outlook.com>
2026-02-04 02:43:55 +0900

    Initial Commit

---
+++ JSM/JSMApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSMApp.swift
+//  JSM
+//
+//  Created by DWGX_Macbook on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct JSMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
